fb10d324546a468883ddc3b6cd66c98d626b4b7a
Felipe Vieira <felipeavieira@outlook.com>
2015-09-04 19:56:10 -0300

    Versão 0.1
    - Entrada de vetor
    - Quicksort

---
+++ Entrada e ordenação quicksort.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void troca(int *v, int a, int b)
+{
+    int temp=v[a];
+    v[a]=v[b];
+    v[b]=temp;
+}
+void quicksort (int *v,int f, int l)
+{
+    if(f>=l)
+    {
+        return;
+    }
+    int p=rand()%(l-f+1)+f;
+    troca(v,p,f);
+    p=f;
+    int i=f;
+    int j=l;
+    while(i<j)
+    {
+        while (v[i]<=v[p]&&i<=l)
+        {
+            i++;
+        }
+        while (v[j]>v[p])
+        {
+            j--;
+        }
+        if (j>i)
+        {
+            troca(v,i,j);
+        }
+    }
+    troca(v,j,p);
+    quicksort(v,f,j-1);
+    quicksort(v,j+1,l);
+}
+void main()
+{
+    int *v,n,i;
+    puts("Ola, quantos valores voce pretende entrar? ");
+    scanf("%d",&n);
+    v=calloc(n,sizeof(int));
+    for(i=0;i<n;i++)
+    {
+        printf("Entre com um numero para a posicao %d: ",i);
+        scanf("%d",&v[i]);
+    }
+    quicksort(v,0,n-1);
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",v[i]);
+    }
+}
