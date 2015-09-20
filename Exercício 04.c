1bd87e35d9d40ab801eb5f82af5a53b87fab37c5
Felipe Vieira <felipeavieira@outlook.com>
2015-09-20 12:30:23 -0300

    Versão 0.7
    -Exercício quatro funcionando

--- Exercício 04.c
+++ Exercício 04.c
@@ -1,46 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* A logica usada nessa resolucao eh a seguinte: se pegarmos o ponto na extrema esquerda, ele estara no fecho.
-A partir dai, procuraremos pontos b que facam com que a sequencia anterior->b->c seja sempre no no sentido antihorario ou colinear,
-para qualquer ponto c. Quando chegarmos ao inicio teremos um fecho fechado*/
 struct coord {int x; int y; int fecho};
 int sentido (struct coord a, struct coord b, struct coord c)
 {
     int i =(b.y - a.y)*(c.x - b.x)-(b.x - a.x)*(c.y - b.y);
-    // A expressao ira dizer se o sentido a->b->c esta no horario, anti-horario, ou se são colineares.
-    if (i<0)
-    {
-        return 0;//Esta no anti-horario.
-    }
+    if (i<0) return 2;//Esta no anti-horario.
     else
-    {
-     if (i>0)
-    {
-        return 1;//Esta no horario.
-    }
-    else
-    {
-        return 2;//Sao colineares.
-    }
-    }
+    if (i>0) return 1;//Esta no horario.
+    else return 0;//Sao colineares.
 }
-void fecho convexo(struct coord * p,int n)
+void fecho_convexo(struct coord * p,int n)
 {
     int i,j,k,l;
-    for (int i = 1; i < n; i++)//Para achar o ponto na extrema esquerda.
+    if (n<3)//não teremos um poligono, entao todos os pontos estao no fecho
     {
-        if (p[i].x < p[k].x)
-        k = i;
+        for (i=0;i<n;i++)
+        {
+            p[i].fecho=1;
+            printf("oi");
+        }
+        return;
     }
-    p[k].fecho=1;
+    k=0;
+    for (i = 1; i< n; i++)//Para achar o ponto na extrema esquerda mais a baixo.
+    {
+        if (p[i].x < p[k].x)k=i;
+        if (p[i].x==p[k].x&&p[i].y<p[k].y)k=i;
+    }
+    j=k;
     do
     {
-    for (i=0;i<n;i++)
-    {
-
+        l=(j+1)%n;//vai para o proximo, volta para o 0 se l for igual a n-1
+        for (i=0;i<n;i++)
+        {
+            if (sentido(p[j], p[i], p[l]) == 2)
+                l = i;
+        }
+        p[j].fecho=1;
+        j=l;
     }
-    }
-    while(l!=k)
+    while (j != k);
 }
 void main()
 {
@@ -48,12 +47,6 @@ void main()
     struct coord * p;
     printf("Ola, resolveremos o problema do \"Fecho Convexo\".\nQuantos pontos voce gostaria de entrar? ");
     scanf("%d",&n);
-    if (n<3)// Saida no caso de nao formar um poligono.
-    {
-        printf("Voce nao pode fazer um fecho convexo com menos de 3 pontos!");
-        getch();
-        return;
-    }
     p = (struct coord*)malloc(n*sizeof(struct coord));
     for (i=0;i<n;i++)
     {
@@ -61,6 +54,15 @@ void main()
         scanf("%d",&p[i].x);
         printf("Coordenada y: ",i);
         scanf("%d",&p[i].y);
-        p[i].fecho=0;//Indicara se esta ou nao no fecho.
+        p[i].fecho=0;
+    }
+    fecho_convexo(p,n);
+    printf("Os pontos do fecho convexo sao:\n");
+    for (i=0;i<n;i++)
+    {
+        if (p[i].fecho==1)
+        {
+            printf("(%d,%d)\n",p[i].x,p[i].y);
+        }
     }
 }
